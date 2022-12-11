791315849b8af623de88668464229c79b765fb11
Rafael Hendrajat Widyadana <106363800+RafaelHendrajat@users.noreply.github.com>
2022-12-11 20:20:34 +0700

    connect database

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import path from 'path';
+import mysql from 'mysql';
 import * as url from 'url';
     const __filename = url.fileURLToPath(import.meta.url);
     const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const PORT = 8080;
 const app = express();
+
+const pool = mysql.createPool({
+    host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'gameofthrones',
+    dateStrings: true,
+    connectionLimit: 10
+});
 
 app.set('view engine', 'ejs')
 
@@ -16,19 +26,42 @@ path.join(__dirname, 'views/Profile')
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
+const dbConnect = () => {
+    return new Promise ((resolve, rejects) => {
+        pool.getConnection((err, conn) => {
+            if (err)
+            {
+               rejects(err);
+            }
+            else
+            {
+                resolve(conn);
+            }
+        })
+    })
+};
+
+app.get('/', async (req, res) => {
+    const conn = await dbConnect();
+    conn.release();
     res.render('home');
 });
 
-app.get('/grafikbar', (req, res) => {
+app.get('/grafikbar', async (req, res) => {
+    const conn = await dbConnect();
+    conn.release();
     res.render('grafikbar');
 });
 
-app.get('/graftakberarah', (req, res) => {
+app.get('/graftakberarah', async (req, res) => {
+    const conn = await dbConnect();
+    conn.release();
     res.render('graftakberarah');
 });
 
-app.get('/pencarian', (req, res) => {
+app.get('/pencarian', async (req, res) => {
+    const conn = await dbConnect();
+    conn.release();
     res.render('pencarian');
 });
 
